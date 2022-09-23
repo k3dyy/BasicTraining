598be313670abe10d464dbf532119a79b8e1cbce
k3dyy <113807112+k3dyy@users.noreply.github.com>
2022-09-23 23:51:49 +0300

    Add project files.

---
+++ BasicTraining/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicTraining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1;
+
+            Console.Write("Sayı gir :  ");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi1 < 0)
+            {
+                Console.WriteLine("Negatif");
+            }
+            else if (sayi1 == 0)
+            {
+                Console.WriteLine("Nötr");
+            }
+            else
+            {
+                Console.WriteLine("Pozitif");
+            }
+            Console.ReadKey();
+        }
+    }
+}
