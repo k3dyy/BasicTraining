5111e5876992fed90d526a715737d96da6037b3e
k3dyy <113807112+k3dyy@users.noreply.github.com>
2022-09-24 01:13:05 +0300

    5 ile 3 sayısının dört işlem örneği

---
+++ Math/Dortislem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    public class Dortislem
+    {
+        public void Topla(int sayi1, int sayi2)
+        {
+            int topla = sayi1 + sayi2;
+            Console.WriteLine("Toplayınca Sonuç = " + topla);
+        }
+        public void Cikar(int sayi1, int sayi2)
+        {
+            int cikar = sayi1 - sayi2;
+            Console.WriteLine("Çıkarınca Sonuç = " + cikar);
+        }
+
+        public void Carpma (int sayi1, int sayi2)
+        {
+            int carpma = sayi1 * sayi2;
+            Console.WriteLine("Çarpınca sonuç = " + carpma);
+        }
+        public void Bolme (double sayi1 , double sayi2)
+        {
+            double bolme = sayi1 / sayi2;
+            Console.WriteLine("Bölünce sonuç = " + bolme);
+        }
+    }
+}
---
+++ Math/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("5 ile 3 ün dört işlemi aşağıdaki gibidir ; ");
+            Dortislem dortIslem = new Dortislem();
+            dortIslem.Topla(5, 3);
+            dortIslem.Cikar(5, 3);
+            dortIslem.Carpma(5, 3);
+            dortIslem.Bolme(5, 3);
+
+            Console.ReadKey();
+        }
+    }
+}
